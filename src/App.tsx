59f3fcb5934732eb92a042ff30ed0dee4ec3d51f
Victor Augusto <victtoru@gmail.com>
2025-08-28 22:04:34 -0300

    Tentativa de feed (Incompleta)

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,15 @@ import { Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage.tsx";
 import DicasPage from "./pages/DicasPage.tsx";
 import SobrePage from "./pages/SobrePages.tsx";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
+  alert("Teste");
   return (
-    <div>
-      <nav>
-        <ul>
-          <li><Link to="/">Início</Link></li>
-          <li><Link to="/dicas">Dicas</Link></li>
-          <li><Link to="/sobre">Sobre</Link></li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dicas" element={<DicasPage />} />
-        <Route path="/sobre" element={<SobrePage />} />
-      </Routes>
-    </div>
+    <>
+      <h1>Teste</h1>
+    </>
   );
 };
 
